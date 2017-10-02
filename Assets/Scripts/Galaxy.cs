f37bf39cda26cb0c1dec4238e0febcb24ea6290a
Florian Hoetzinger <Florian.Hoetzinger@gab-net.com>
2017-10-02 16:39:39 +0200

    startdata and minradius / ring galaxy

--- Assets/Scripts/Galaxy.cs
+++ Assets/Scripts/Galaxy.cs
@@ -5,13 +5,24 @@ public class Galaxy : MonoBehaviour
     public int numberOfStars = 300;
     public int maximumRadius = 100;
     public float minDistBetweenStars;
+    public int minRadius = 0;
     // Use this for initialization
     private void Start()
     {
         int failCount = 0;
+
+        if (minRadius>maximumRadius)
+        {
+            int temp = maximumRadius;
+            maximumRadius = minRadius;
+            minRadius = temp;
+        }
+
         for (int i = 0; i < numberOfStars; i++)
         {
-            float distance = Random.Range(0, maximumRadius);
+            Star starData = new Star("Star" + i, Random.Range(1, 10));
+
+            float distance = Random.Range(minRadius, maximumRadius);
             float angle = Random.Range(0, 2 * Mathf.PI);
 
             Vector3 cartPosition = new Vector3(distance * Mathf.Cos(angle), 0, distance * Mathf.Sin(angle));
@@ -21,6 +32,7 @@ public class Galaxy : MonoBehaviour
             if (positionCollider.Length == 0)
             {
                 var starGO = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                starGO.name = starData.starName;
                 starGO.transform.position = cartPosition;
                 failCount = 0;
             }
---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star {
+    public string starName { get; protected set; }
+    public int numberOfPlanets { get; protected set; }
+
+    public Star(string name, int planets)
+    {
+        this.starName = name;
+        this.numberOfPlanets = planets;
+    }
+
+}
